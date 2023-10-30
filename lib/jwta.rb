d77ace473e016c567e520b107b5fb3f4bea984c5
Gerard Montemayor <me@grdmnt.com>
2023-10-31 01:54:58 +1100

    Initialize gem project

---
+++ lib/jwta.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jwta/version"
+
+module JWTA
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jwta_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JWTA do
+  it "has a version number" do
+    expect(JWTA::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
